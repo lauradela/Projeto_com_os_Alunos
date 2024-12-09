cd7256f894e3a8cf08a5cd2b013bfc3d2b66aec2
ArlessonMoura <arlessonmss@gmail.com>
2024-12-09 20:37:46 -0300

    btns add

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const shoppingKart = () => {
   console.log('Ir para o carrinho');
 };
+
+const decrementIncrement = (id, operation) => {
+  const qtdPizza = document.getElementById(id);
+
+  if (operation === 'plus') {
+    qtdPizza.innerText = parseInt(qtdPizza.innerText) + 1;
+    console.log(qtdPizza);
+    return;
+  } else {
+    if (parseInt(qtdPizza.innerText) > 0) {
+      qtdPizza.innerText = parseInt(qtdPizza.innerText) - 1;
+      console.log(qtdPizza);
+      return;
+    }
+    console.log(qtdPizza);
+  }
+};
