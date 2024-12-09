791a6487aade5cc2c7da8c9fb86d48aae0461744
ArlessonMoura <arlessonmss@gmail.com>
2024-12-09 19:22:54 -0300

    no features.

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 const shoppingKart = () => {
   console.log('Ir para o carrinho');
 };
-
-const oneMore = (target, operation) => {
-  const value = document.getElementById('value');
-  if (operation === 'plus') {
-    value.innerText = parseInt(value.innerText) + 1;
-  } else {
-    value.innerText = parseInt(value.innerHTML) - 1;
-  }
-};
